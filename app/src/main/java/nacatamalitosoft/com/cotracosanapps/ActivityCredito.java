435f7edf77a6ee52580859867a00b1b9566b4f9d
Mike Antonio Ortiz Blandon <martiz-27@hotmail.com>
2019-07-18 14:04:20 -0600

    Probando la funcionalidad del gridview

--- app/src/main/java/nacatamalitosoft/com/cotracosanapps/ActivityCredito.java
+++ app/src/main/java/nacatamalitosoft/com/cotracosanapps/ActivityCredito.java
@@ -5,18 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Map;
 
 import nacatamalitosoft.com.cotracosanapps.Modelos.Credito;
 import nacatamalitosoft.com.cotracosanapps.Modelos.DetalleDeCredito;
@@ -34,6 +35,8 @@ public class ActivityCredito extends AppCompatActivity {
 
         recyclerView  = (RecyclerView) findViewById(R.id.ReciclerId);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+
     }
 
     public class getCredito extends AsyncTask<Void, Void, Void>{
@@ -50,24 +53,64 @@ public class ActivityCredito extends AppCompatActivity {
                 public void onResponse(String response) {
                     try{
 
+                        //Obteniendo el objeto de la respuesta JSON
                         JSONObject object = new JSONObject(response);
-                        temp.setId(object.getInt("Id"));
-                        temp.setCodigoCredito(object.getString("CodigoCredito"));
-                        temp.setFecha(Date.valueOf(object.getString("Fecha")));
-                        temp.setMontoTotal(object.getDouble("MontoTotal"));
-                        temp.setTotalAbonado(object.getDouble("TotalAbonado"));
-                        temp.setNumeroAbonos(object.getInt("NumeroAbonos"));
-                        temp.setCreditoAnulado(object.getBoolean("CreditoAnulado"));
-                        temp.setEstadoCredito(object.getBoolean("EstadoDeCredito"));
 
-                        JSONObject objecDet = new JSONObject((Map) object.getJSONObject("DetallesDeCreditos"));
-                        tempDet.setId(objecDet.getInt("Id"));
-                        tempDet.setArticuloId(objecDet.getInt("ArticuloId"));
-                        tempDet.setCodigoArticulo(objecDet.getString("CodigoArticulo"));
-                        tempDet.setNombreArticulo(objecDet.getString("Articulo"));
-                        tempDet.setCantidad(objecDet.getInt("Cantidad"));
-                        tempDet.setPrecio(objecDet.getDouble("Precio"));
-                        listaDetalle.add(tempDet);
+                        //Convirtiendo la respuesta a un arreglo
+                        JSONArray jsonArray = object.getJSONArray("Creditos");
+
+
+                        //Inicializando la nueva lista de creditos
+                        listaInterna = new ArrayList<>();
+
+                        //Recorriendo el arreglo de creditos
+                        for(int i=0; i<jsonArray.length();i++)
+                        {
+                            //Obteniendo el objeto del arreglo en la posicion i
+                            JSONObject obj = jsonArray.getJSONObject(i);
+
+                            //Convirtiendo en arreglo la lista de detalles
+                            JSONArray objDet = obj.getJSONArray("DetallesDeCreditos");
+
+                            //Inicializando la lista de detalles
+                            listaDetalle = new ArrayList<>();
+
+                            //Recorriendo la lista de detalles
+                            for(int j =0; j<objDet.length();j++)
+                            {
+                                //Obteniendo el objeto del detalle en la posicion j
+                                JSONObject det =  objDet.getJSONObject(j);
+
+                                //Creando un nuevo objeto de detalle
+                                tempDet = new DetalleDeCredito(det.getInt("Id"),
+                                        det.getInt("ArticuloId"), det.getString("CodigoArticulo"),
+                                        det.getString("Articulo"), det.getInt("Cantidad"),
+                                        det.getDouble("Precio"));
+
+                                //Agregando a lista de detalles
+                                listaDetalle.add(tempDet);
+                            }
+
+                            //Creando un nuevo objeto de Credito
+                            temp = new Credito(obj.getInt("Id"), obj.getString("CodigoCredito"),
+                                    Date.valueOf(obj.getString("Fecha")), obj.getDouble("MontoTotal"),
+                                    obj.getDouble("TotalAbonado"), obj.getInt("NumeroAbonos"),
+                                    obj.getBoolean("CreditoAnulado"), obj.getBoolean("EstadoDeCredito"),
+                                    listaDetalle);
+
+
+                            //Agregando a la lista de Creditos
+                            listaInterna.add(temp);
+                        }
+
+                        if(listaInterna.size() ==0)
+                            Toast.makeText(getBaseContext(), "No hay Creditos", Toast.LENGTH_LONG ).show();
+                        else
+                        {
+                            AdapterCreditosBus credito = new AdapterCreditosBus(listaInterna);
+                            recyclerView.setAdapter(credito);
+                        }
+
 
 
 
@@ -79,7 +122,7 @@ public class ActivityCredito extends AppCompatActivity {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-
+                    Toast.makeText(getBaseContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
             return null;
