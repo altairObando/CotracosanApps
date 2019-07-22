cbc1a10c2bc90706939a51c8eb40605a65a52b23
altairObando <pxnfilo@gmail.com>
2019-07-22 11:52:29 -0600

    Agregando Actividades para mostrar los detalles de el usuario en sesion.

--- app/src/main/java/nacatamalitosoft/com/cotracosanapps/UserManager/ImagePreviewActivity.java
+++ app/src/main/java/nacatamalitosoft/com/cotracosanapps/UserManager/ImagePreviewActivity.java
@@ -1,15 +1,45 @@
 package nacatamalitosoft.com.cotracosanapps.UserManager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import nacatamalitosoft.com.cotracosanapps.R;
+import nacatamalitosoft.com.cotracosanapps.localDB.UserSingleton;
 
 public class ImagePreviewActivity extends AppCompatActivity {
-
+    Bitmap imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_preview);
+        // Barra para retornar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        loadImagenPerfil();
+    }
+
+    private void loadImagenPerfil() {
+
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/nacatamalitosoft/com/cotracosanapps/UserManager/UserDetailsActivity.java
+++ app/src/main/java/nacatamalitosoft/com/cotracosanapps/UserManager/UserDetailsActivity.java
@@ -1,21 +1,39 @@
 package nacatamalitosoft.com.cotracosanapps.UserManager;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import nacatamalitosoft.com.cotracosanapps.R;
+import nacatamalitosoft.com.cotracosanapps.Web.VolleySingleton;
 import nacatamalitosoft.com.cotracosanapps.localDB.User;
 import nacatamalitosoft.com.cotracosanapps.localDB.UserSingleton;
 
@@ -47,23 +65,60 @@ public class UserDetailsActivity extends AppCompatActivity {
         optionsCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Tomar una foto
-
+                final Dialog dialog = new Dialog(UserDetailsActivity.this);
+                dialog.setContentView(R.layout.dialog_chosser);
+                dialog.setTitle("Foto del perfil");
+                LinearLayout rowCamara = dialog.findViewById(R.id.dialogCamara);
+                LinearLayout rowGaleria = dialog.findViewById(R.id.dialogGaleria);
+                rowCamara.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Tomar una foto
+                        Intent tomarFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(tomarFoto, 1);
+                        dialog.dismiss();
+                    }
+                });
+                rowGaleria.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // seleccionar desde la galeria
+                        Intent seleccionarImagen = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(seleccionarImagen, 2);
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
         // Lanzar la activdad para el cambio de la contraseña.
         cambiarContrasenia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Cargar el intent con el cambio de contraseña.
+                Intent cambio = new Intent(UserDetailsActivity.this, ChangePasswordActivity.class);
+                startActivity(cambio);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode>=1 && requestCode <=2){
+            if(resultCode == RESULT_OK){
+                Uri selectedImage = data.getData();
+                imagenPerfil.setImageURI(selectedImage);
+                // Obteniendo el bmp
+                BitmapDrawable drawable = (BitmapDrawable) imagenPerfil.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                new updateUserImage(bitmap).execute();
+            }
+        }
     }
 
     private void setValues() {
         // Cargar la imagen del perfil
-        String base64 = currentUser.getAvatar();
+        String base64 = currentUser.getAvatar().split(",")[1];
         byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         imagenPerfil.setImageBitmap(bitmap);
@@ -93,5 +148,41 @@ public class UserDetailsActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public class updateUserImage extends AsyncTask<Void, Void, Void>{
+        private Bitmap bitmap;
+        public updateUserImage(Bitmap bitmap){
+            this.bitmap = bitmap;
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            byte[] bytes = outputStream.toByteArray();
+            final String temp = Base64.encodeToString(bytes, Base64.DEFAULT);
+            StringRequest request = new StringRequest(Request.Method.POST, "http://cotracosan.tk/ApiAccount/CambiarImagen", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(UserDetailsActivity.this, "Imagen actualizada", Toast.LENGTH_LONG).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(UserDetailsActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    super.getParams();
+                    Map<String, String> params = new HashMap<>();
+                    params.put("idUsuario", currentUser.getId());
+                    params.put("imagen", "data:image/jpeg;base64,"+temp);
+                    return params;
+                }
+            };
+
+            VolleySingleton.getInstance(UserDetailsActivity.this).addToRequestQueue(request);
+            return null;
+        }
     }
 }
