b5ab97354c7d9903738ab46c6bb10bfa44420906
Mike Antonio Ortiz Blandon <martiz-27@hotmail.com>
2019-07-25 16:08:26 -0600

    Casi terminado el modulo de Carreras

--- app/src/main/java/nacatamalitosoft/com/cotracosanapps/ActivityDetalleCarrera.java
+++ app/src/main/java/nacatamalitosoft/com/cotracosanapps/ActivityDetalleCarrera.java
@@ -17,12 +17,13 @@ public class ActivityDetalleCarrera extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        carreras = (Carreras)getIntent().getSerializableExtra("carrera");
+
 
 
         setContentView(R.layout.activity_detalle_carrera);
+        carreras = (Carreras)getIntent().getSerializableExtra("carrera");
         codigoCarrera = (TextView)findViewById(R.id.CodigoCarrera);
-        fechaCarrera = (TextView)findViewById(R.id.FechaCarrera);
+        fechaCarrera = (TextView)findViewById(R.id.FechaDeCarrera);
         placa = (TextView)findViewById(R.id.Placa);
         montoRecaudado=(TextView)findViewById(R.id.MontoRecaudado);
         multa = (TextView)findViewById(R.id.Multa);
@@ -32,7 +33,17 @@ public class ActivityDetalleCarrera extends AppCompatActivity {
         llegada = (TextView)findViewById(R.id.HoraLlegada);
 
         if(carreras!=null)
-            setData();
+        {
+            codigoCarrera.setText(carreras.getCodigoCarrera());
+            fechaCarrera.setText(carreras.getFechaDeCarrera().replace("/", "-"));
+            placa.setText(carreras.getVehiculo());
+            montoRecaudado.setText(String.valueOf(carreras.getMontoRecaudado()));
+            multa.setText(String.valueOf(carreras.getMulta()));
+            montoRestante.setText(String.valueOf(carreras.getMontoRestante()));
+            lugarFinal.setText(carreras.getLugarFinalRecorrido());
+            turno.setText(carreras.getTurno());
+            llegada.setText(carreras.getHoraDeLlegada());
+        }
         else
             Toast.makeText(ActivityDetalleCarrera.this, "Carrera no encontrada", Toast.LENGTH_LONG).show();
     }
